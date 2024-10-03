e2ce26b3072e3977f1f6be064bb174005a6fade4
Vlad Karsten <vkarsten1111@icloud.com>
2024-10-03 10:05:53 +0700

    Changed: gitignore, readme, requirements, secret_key in settings

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -10,9 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
-from decouple import config
 import os
+from pathlib import Path
+
+
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+# SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'XXX'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
@@ -91,8 +95,8 @@ DATABASES = {
 
 
 def basic_password_validator(class_name, **kwargs):
-    MODULE_PATH = "django.contrib.auth.password_validation"
-    validator = {"NAME": ".".join([MODULE_PATH, class_name])}
+    module_path = "django.contrib.auth.password_validation"
+    validator = {"NAME": ".".join([module_path, class_name])}
     validator.update(kwargs)
     return validator
 
