37db9d7235207ff17b7afda0001a18cf7cbb4fb3
Vlad Karsten <vkarsten1111@icloud.com>
2024-10-07 23:25:08 +0700

    Feat: add homepage, about, catalog and catalog/n urls

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('homepage.urls')),
+    path('about/', include('about.urls')),
+    path('catalog/', include('catalog.urls')),
     path('admin/', admin.site.urls),
 ]
