f14e771cf8c1d6738f4a6f95f36162e8d852e792
Vlad Karsten <vkarsten1111@icloud.com>
2024-10-08 15:29:44 +0700

    Test(catalog): cover re and converter with tests

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -9,6 +9,32 @@ class CatalogHttpResponseTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_item_from_catalog_status_code(self):
-        id = randint(1, 100)
+        id = randint(0, 100)
         response = Client().get(f'/catalog/{id}/')
         self.assertEqual(response.status_code, 200)
+
+    def test_re_item_from_catalog_status_code(self):
+        id = randint(1, 100)
+        response = Client().get(f'/catalog/re/{id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(f'{id}'.encode('utf-8'), response.content)
+        id = -1
+        response = Client().get(f'/catalog/re/{id}/')
+        self.assertEqual(response.status_code, 404)
+        response = Client().get('/catalog/re/')
+        self.assertEqual(response.status_code, 404)
+        response = Client().get('/catalog/re/qerty/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_converter_item_from_catalog_status_code(self):
+        id = randint(1, 100)
+        response = Client().get(f'/catalog/converter/{id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(f'{id}'.encode('utf-8'), response.content)
+        id = -1
+        response = Client().get(f'/catalog/converter/{id}/')
+        self.assertEqual(response.status_code, 404)
+        response = Client().get('/catalog/converter/')
+        self.assertEqual(response.status_code, 404)
+        response = Client().get('/catalog/converter/qerty/')
+        self.assertEqual(response.status_code, 404)
