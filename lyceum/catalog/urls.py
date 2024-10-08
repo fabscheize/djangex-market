90a4fc81563d6690c046b62d585e9cb3bd9144c9
Vlad Karsten <vkarsten1111@icloud.com>
2024-10-08 15:14:19 +0700

    Feat(catalog): Add re/n and converter/n paths for positive integer n

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 
-from . import views
+from . import converters, views
 
+register_converter(converters.PositiveIntegerConverter, 'uint')
 
 urlpatterns = [
     path('', views.item_list),
     path('<int:id>/', views.item_detail),
+    re_path(r'^re/(?P<id>[1-9]\d*)/$', views.re_item_detail),
+    path('converter/<uint:id>/', views.converter_item_detail),
 ]
--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -7,3 +7,11 @@ def item_list(request):
 
 def item_detail(request, id):
     return HttpResponse('<body>Подробно элемент</body>')
+
+
+def re_item_detail(request, id):
+    return HttpResponse(f'<body>{id}</body>')
+
+
+def converter_item_detail(request, id):
+    return HttpResponse(f'<body>{id}</body>')
