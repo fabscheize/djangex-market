d96c83185b48ddfd9838b91302b205f0a3ae831e
Vlad Karsten <vkarsten1111@icloud.com>
2024-10-08 14:35:26 +0700

    Feat: Add coffee page

--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from django.test import Client, TestCase
 
 
@@ -5,3 +7,8 @@ class HomeHttpResponseTest(TestCase):
     def test_homepage_status_code(self):
         response = Client().get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_coffee_status_code(self):
+        response = Client().get('/coffee/')
+        self.assertEqual(response.status_code, HTTPStatus.IM_A_TEAPOT)
+        self.assertIn('Я чайник'.encode('utf-8'), response.content)
--- lyceum/homepage/urls.py
+++ lyceum/homepage/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path('', views.home)]
+urlpatterns = [
+    path('', views.home),
+    path('coffee/', views.coffee),
+]
