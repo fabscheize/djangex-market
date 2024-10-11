f4b73c222032888ea079f27cc1be7a2244e82095
Vlad Karsten <vkarsten1111@icloud.com>
2024-10-11 14:18:41 +0700

    Fix: change relative paths to absolute
    Fixes #7

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, re_path, register_converter
 
-from . import converters, views
+from catalog import converters, views
+
 
 register_converter(converters.PositiveIntegerConverter, 'uint')
 
 urlpatterns = [
     path('', views.item_list),
-    path('<int:id>/', views.item_detail),
-    re_path(r'^re/(?P<id>[1-9]\d*)/$', views.re_item_detail),
-    path('converter/<uint:id>/', views.converter_item_detail),
+    path('<int:pk>/', views.item_detail),
+    re_path(r'^re/(?P<pk>[1-9]\d*)/$', views.re_item_detail),
+    path('converter/<uint:pk>/', views.converter_item_detail),
 ]
