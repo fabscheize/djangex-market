3d8f581063f21161f2c9ff0b2b9d135f25e74474
Vlad Karsten <vkarsten1111@icloud.com>
2024-10-16 23:42:46 +0700

    Feat: Set up administration of catalog app

---
+++ lyceum/catalog/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+
+from catalog import models
+
+
+@admin.register(models.Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        models.Item.id.field.name,
+        models.Item.name.field.name,
+        models.Item.is_published.field.name,
+    )
+    list_editable = (models.Item.is_published.field.name,)
+    list_display_links = (models.Item.name.field.name,)
+    filter_horizontal = (models.Item.tags.field.name,)
+
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        models.Tag.name.field.name,
+        models.Tag.is_published.field.name,
+    )
+    list_editable = (models.Tag.is_published.field.name,)
+    list_display_links = (models.Tag.name.field.name,)
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        models.Category.name.field.name,
+        models.Category.is_published.field.name,
+    )
+    list_editable = (models.Category.is_published.field.name,)
+    list_display_links = (models.Category.name.field.name,)
