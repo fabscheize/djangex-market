4a7b0a56ae9a0a734246ec616fae8a866e883ef2
Vlad Karsten <vkarsten1111@icloud.com>
2024-10-17 13:16:20 +0700

    Fix: /coffee test in homepage

--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -1,21 +1,17 @@
 from http import HTTPStatus
 
 from django.test import Client, TestCase
-from parametrize import parametrize
 
 
 class HomeHttpResponseTest(TestCase):
     def setUp(self):
         self.client = Client()
 
-    @parametrize(
-        'path, expected_content, status',
-        [
-            ('', 'Главная', HTTPStatus.OK),
-            ('coffee/', 'Я чайник', HTTPStatus.IM_A_TEAPOT),
-        ],
-    )
-    def test_homepage_response(self, path, expected_content, status):
-        response = self.client.get('/' + path)
-        self.assertEqual(response.status_code, status)
-        self.assertIn(expected_content.encode('utf-8'), response.content)
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_coffee_status_code(self):
+        response = self.client.get('/coffee/')
+        self.assertEqual(response.status_code, HTTPStatus.IM_A_TEAPOT)
+        self.assertIn('Я чайник'.encode('utf-8'), response.content)
