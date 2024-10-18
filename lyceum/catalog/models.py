50e91d9b07b809857d20627fa8bde313d209ea72
Vlad Karsten <vkarsten1111@icloud.com>
2024-10-18 20:36:08 +0700

    Feat: Add normalized_name field to Category and Tag

--- lyceum/catalog/models.py
+++ lyceum/catalog/models.py
@@ -1,31 +1,57 @@
-import django.core.exceptions
-import django.core.validators
-import django.db
+import re
+
+from django.core import exceptions, validators
+from django.db import models
 
 from catalog.validators import ValidateContainsWords
 from core.models import AbstractModel
 
+PUNCTUATION_REGEX = re.compile(r'[\W_]')
+
+
+def normalize_name(name):
+    name = re.sub(PUNCTUATION_REGEX, '', name).lower()
+    name = name.translate(str.maketrans('авекмнорстух', 'abekmhopctyx'))
+    return name
+
 
 class Category(AbstractModel):
-    slug = django.db.models.CharField(
+    slug = models.CharField(
         verbose_name='слаг',
         max_length=200,
         validators=[
-            django.core.validators.validate_slug,
+            validators.validate_slug,
         ],
         unique=True,
         help_text=(
             'Только латинские буквы, цифры, знаки подчеркивания или дефиса'
         ),
     )
-    weight = django.db.models.IntegerField(
+    weight = models.IntegerField(
         verbose_name='вес',
         default=100,
         validators=[
-            django.core.validators.MinValueValidator(1),
-            django.core.validators.MaxValueValidator(32767),
+            validators.MinValueValidator(1),
+            validators.MaxValueValidator(32767),
         ],
     )
+    normalized_name = models.CharField(
+        verbose_name='нормализованное имя',
+        max_length=150,
+        unique=True,
+        editable=False,
+    )
+
+    def clean(self):
+        normalized = normalize_name(self.name)
+        if Category.objects.filter(normalized_name=normalized).exists():
+            raise exceptions.ValidationError(
+                {'name': 'Категория с похожим именем уже существует.'},
+            )
+
+    def save(self, *args, **kwargs):
+        self.normalized_name = normalize_name(self.name)
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = 'категория'
@@ -33,17 +59,34 @@ class Category(AbstractModel):
 
 
 class Tag(AbstractModel):
-    slug = django.db.models.CharField(
+    slug = models.CharField(
         verbose_name='слаг',
         max_length=200,
         validators=[
-            django.core.validators.validate_slug,
+            validators.validate_slug,
         ],
         unique=True,
         help_text=(
             'Только латинские буквы, цифры, знаки подчеркивания или дефиса'
         ),
     )
+    normalized_name = models.CharField(
+        verbose_name='нормализованное имя',
+        max_length=150,
+        unique=True,
+        editable=False,
+    )
+
+    def clean(self):
+        normalized = normalize_name(self.name)
+        if Tag.objects.filter(normalized_name=normalized).exists():
+            raise exceptions.ValidationError(
+                {'name': 'Тег с похожим именем уже существует.'},
+            )
+
+    def save(self, *args, **kwargs):
+        self.normalized_name = normalize_name(self.name)
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = 'тег'
@@ -52,18 +95,18 @@ class Tag(AbstractModel):
 
 class Item(AbstractModel):
 
-    category = django.db.models.ForeignKey(
+    category = models.ForeignKey(
         Category,
-        on_delete=django.db.models.CASCADE,
+        on_delete=models.CASCADE,
         verbose_name='категория',
     )
 
-    tags = django.db.models.ManyToManyField(
+    tags = models.ManyToManyField(
         Tag,
         verbose_name='теги',
     )
 
-    text = django.db.models.TextField(
+    text = models.TextField(
         verbose_name='текст',
         help_text=(
             'В описании обязательно должно быть слово '
