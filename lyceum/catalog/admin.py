302f19e74c2c2e5693fb365b8c6a072f8461195f
Vlad Karsten <vkarsten1111@icloud.com>
2024-10-25 16:06:58 +0700

    Feat(admin): Add ability to view and change images of Item model

--- lyceum/catalog/admin.py
+++ lyceum/catalog/admin.py
@@ -1,18 +1,29 @@
-from django.contrib import admin
-
 from catalog import models
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+from sorl.thumbnail import get_thumbnail
+
+
+class ItemImageInline(admin.TabularInline):
+    model = models.ItemImage
+    extra = 0
+    fields = (models.ItemImage.get_image, 'image')
+    readonly_fields = (models.ItemImage.get_image,)
 
 
 @admin.register(models.Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = (
-        models.Item.id.field.name,
         models.Item.name.field.name,
         models.Item.is_published.field.name,
+        models.Item.get_main_image,
     )
     list_editable = (models.Item.is_published.field.name,)
     list_display_links = (models.Item.name.field.name,)
     filter_horizontal = (models.Item.tags.field.name,)
+    inlines = [ItemImageInline]
+    readonly_fields = (models.Item.get_main_image,)
+
 
 
 @admin.register(models.Tag)
@@ -32,4 +43,5 @@ class CategoryAdmin(admin.ModelAdmin):
         models.Category.is_published.field.name,
     )
     list_editable = (models.Category.is_published.field.name,)
+    list_display_links = (models.Category.name.field.name,)
     list_display_links = (models.Category.name.field.name,)
