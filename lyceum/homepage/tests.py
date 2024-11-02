3b34f1e2951716c47cb48cc500c4aba308282609
Vlad Karsten <vkarsten1111@icloud.com>
2024-11-02 20:03:28 +0700

    Tests: Minimize tests

--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -1,9 +1,25 @@
+from http import HTTPStatus
+
 from django.test import Client, TestCase
 from django.urls import reverse
 
 from catalog.models import Category, Item, Tag
 
 __all__ = []
+
+
+class HomepageHttpResponseTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_homepage_status_code(self):
+        response = self.client.get(reverse('homepage:home'))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_coffee_status_code(self):
+        response = self.client.get(reverse('homepage:coffee'))
+        self.assertEqual(response.status_code, HTTPStatus.IM_A_TEAPOT)
+        self.assertEqual('Я чайник'.encode('utf-8'), response.content)
 
 
 class HomepageContextTest(TestCase):
