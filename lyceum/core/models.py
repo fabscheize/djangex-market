b96e45ecf484c15d1edfd09917ffc28a1f0f6b76
Vlad Karsten <vkarsten1111@icloud.com>
2024-11-04 19:06:18 +0700

    Revert "Fix: Verbose names"
    This reverts commit fbf0132f3b44b6625e1c4b70d798b81cffd509c0.

--- lyceum/core/models.py
+++ lyceum/core/models.py
@@ -16,14 +16,14 @@ def image_directory_path(instance, filename):
 
 class BaseSaleModel(django.db.models.Model):
     name = django.db.models.CharField(
-        verbose_name='название',
+        verbose_name=_('title'),
         help_text=_('Max 150 symbols'),
         max_length=150,
         unique=True,
     )
 
     is_published = django.db.models.BooleanField(
-        verbose_name='опубликовано',
+        verbose_name=_('published'),
         default=True,
     )
 
