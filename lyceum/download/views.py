42d300c0f2ed35d212355d6e17f6a8c37af6b21b
Vlad Karsten <vkarsten1111@icloud.com>
2024-11-05 11:06:19 +0700

    Fix(download): Add test for the existence of a path

--- lyceum/download/views.py
+++ lyceum/download/views.py
@@ -1,10 +1,12 @@
 from django.conf import settings
-from django.http import FileResponse
+from django.http import FileResponse, Http404
 
 __all__ = []
 
 
 def get_file(request, path):
+    if not path:
+        raise Http404('File not found')
     return FileResponse(
         open(settings.MEDIA_ROOT / path, 'rb'),
         as_attachment=True,
